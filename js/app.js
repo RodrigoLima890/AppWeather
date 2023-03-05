a801eebb127522b27e8620e6094127ee4219eda7
Rodrigo Lima <rodrigodaschagas1234@gmail.com>
2023-03-05 16:34:54 -0300

    Melhorando o codigo

--- js/app.js
+++ js/app.js
@@ -2,30 +2,52 @@ const form = document.querySelector('[data-js = "change-location"]');
 const cityNameContainer = document.querySelector('[data-js=city-name]');
 const cityWeatherContainer = document.querySelector('[data-js=city-weather]');
 const cityTemperatureContainer = document.querySelector('[data-js=city-temperature]');
-const cardContainer = document.querySelector('[data-js=city-card]');
+let cardContainer = document.querySelector('[data-js=city-card]');
 const imgContainer = document.querySelector('[data-js= "time"]');
 const iconContainer = document.querySelector('[data-js="time-icon"]');
+
+const dayOrNight = (boolean)=> boolean ? 
+imgContainer.src='./src/day.svg' : imgContainer.src='./src/night.svg';
+
+const showContainerCard=()=>{
+    if(cardContainer.classList.contains('d-none')) 
+        cardContainer.classList.remove('d-none');
+}
+const getTimeIcon = (WeatherIcon) =>`<img src="./src/icons/${WeatherIcon}.svg"/>`;
+
+const iconInsert=(timeIcon)=> iconContainer.innerHTML = timeIcon;
+
+const nameCityInsert=(nameCity)=> cityNameContainer.textContent = nameCity;
+
+const weatherInsert=(weather)=> cityWeatherContainer.textContent = weather;
+
+const temperatureInsert=(temperature)=> cityTemperatureContainer.textContent = temperature;
 
 form.addEventListener('submit', async event=>{
     event.preventDefault();
 
     const cityName = event.target.city.value;
     const cityData = await getDataCity(cityName);
+
+    //Pegar Dados De City
     const [{Key, LocalizedName}] = cityData;
+
+    //Pegar Dados Do Clima De Uma Cidade 
     const [{WeatherText, Temperature, IsDayTime, WeatherIcon}] = await getDataWeatherCity(Key);
-    
-    const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg"/>`;
 
-    if(cardContainer.classList.contains('d-none')){
-        cardContainer.classList.remove('d-none');
-    }
+    //Obter Icon Correspondente
+    const timeIcon = getTimeIcon(WeatherIcon);
 
-    IsDayTime ? imgContainer.src='./src/day.svg' : imgContainer.src='./src/night.svg';
+    //mostrarCard
+    showContainerCard();
 
-    iconContainer.innerHTML = timeIcon;
-    cityNameContainer.textContent = LocalizedName;
-    cityWeatherContainer.textContent = WeatherText;
-    cityTemperatureContainer.textContent = Temperature.Metric.Value;
+    //Inserir Valores No Container
+    dayOrNight(IsDayTime);
+    iconInsert(timeIcon);
+    nameCityInsert(LocalizedName);
+    weatherInsert(WeatherText);
+    temperatureInsert(Temperature.Metric.Value);
 
+    //Resetar Formulário
     form.reset();
 })
